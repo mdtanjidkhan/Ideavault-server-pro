82eddb5ba886c62f0b08e3c449db308bd6b5b287
mdtanjidkhan <mdtanjidkhan152@gmail.com>
2026-06-21 02:22:37 +0600

    initial: setup express server and connect mongodb database

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 const cors = require('cors')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { createRemoteJWKSet, jwtVerify } = require('jose-cjs');
 const PORT = process.env.PORT
 
 const uri = process.env.MONGODB_SERVER_URL;
@@ -23,28 +24,52 @@ const client = new MongoClient(uri, {
   }
 });
 
+const JWKS = createRemoteJWKSet(
+  new URL(`${process.env.CLIENT_URL}/api/auth/jwks`)
+)
+
+// 
+const verifyToken = async (req, res, next)=>{
+   const authHeader = req?.headers.authorization
+   if(!authHeader){
+    return res.status(401).send({message:"Unauthorization"});
+   }
+   const token = authHeader.split(" ")[1]
+   console.log(token)
+    if(!token){
+      return res.status(401).send({message: 'Unauthorized access'});
+    }
+    try{
+        const {payload}= await jwtVerify(token, JWKS)
+    console.log(payload);
+     next();
+    }catch(error){
+      return res.status(403).send({message: 'Forbidden access'});
+    }
+}
+
 async function run() {
   try {
-    
+
     await client.connect();
-     
+
     const db = client.db("IdeaVault");
     const usersCollection = db.collection("users");
     const usersComment = db.collection("comments")
 
-    // get 
+    // get idea page
 
-    app.get('/users', async(req,res)=>{
+    app.get('/users', verifyToken, async (req, res) => {
       const search = req.query.search;
       const category = req.query.category;
       let query = {};
       // category filter
-      if(category){
+      if (category) {
         query.category = category;
       }
       // 
-      if(search){
-        query.title ={
+      if (search) {
+        query.title = {
           $regex: search,
           $options: "i",
         };
@@ -54,73 +79,126 @@ async function run() {
       // console.log(result);
       res.send(result)
     })
+    // trending-ideas homepage limit jonno
+    app.get('/trending-ideas', async (req, res) => {
+      const result = await usersCollection.find().sort({ _id: -1 }).limit(6).toArray();
+      res.send(result);
+    })
 
-    //  get 
+    //  get comment 
     app.get('/comments', async (req, res) => {
-   const result = await usersComment
-      .find()
-      .sort({ _id: -1 })
-      .toArray();
+      const result = await usersComment
+        .find()
+        .sort({ _id: -1 })
+        .toArray();
 
-   res.send(result);
-});
+      res.send(result);
+    });
 
-//   patch 
- app.patch('/comments/:id', async(req,res)=>{
-   const id = req.params.id
-   const updateData = req.body;
-   const filter ={
-    _id: new ObjectId(id)
-   }
-   const result = await usersComment.updateOne(filter,{$set:updateData});
-   res.send(result);
- })
+    // 
 
+    //   patch comments
+    app.patch('/comments/:id',verifyToken, async (req, res) => {
+      const id = req.params.id
+      const data = req.body;
+      const filter = {
+        _id: new ObjectId(id)
+      }
+      const updataDoc = {
+        $set: {
+          text: data.text
+        }
+      }
+      const result = await usersComment.updateOne(filter, updataDoc);
+      res.send(result);
+    })
 
-
-// delete 
-
+    // delete comment 
+    app.delete('/comments/:id', verifyToken, async (req, res) => {
+      const id = req.params.id;
+      const query = {
+        _id: new ObjectId(id)
+      }
+      const result = await usersComment.deleteOne(query);
+      res.send(result);
+    })
 
 
     // comments jonno post methed
-   
-   app.post('/comments', async(req,res)=>{
-    const data = req.body;
-    if (data && typeof data.text === 'object' && data.text !== null) {
-      data.text = data.text.comments || ""; 
+
+    app.post('/comments', verifyToken, async (req, res) => {
+      const comment = req.body;
+      const result = await usersComment.insertOne(comment);
+      //  console.log("comments data",result);
+      res.send({
+        _id: result.insertedId,
+        ...comment
+      });
+    })
+     
+    //  User Profile Update korar route 
+app.patch('/user-update/:email',verifyToken, async (req, res) => {
+  const email = req.params.email;
+  const filter = { userEmail: email }; 
+  const updatedDoc = {
+    $set: {
+      name: req.body.name,
     }
-     const comment = {
-    ...data,
-    createdAt: new Date(),
   };
-     const result = await usersComment.insertOne(comment);
-    //  console.log("comments data",result);
-     res.send({
-       _id: result.insertedId,
-      ...comment
-     });
-
-   })
-    
-
+  const result = await usersCollection.updateMany(filter, updatedDoc); 
+  res.send(result);
+});
+   
     //  get params id
-    
-    app.get('/users/:id', async(req,res)=>{
-        const id = req.params.id;
-        const query={
-          _id: new ObjectId(id)
-        };
-        const result = await usersCollection.findOne(query);
-        // console.log(result)
-        res.send(result);
+
+    app.get('/users/:id',verifyToken, async (req, res) => {
+      const id = req.params.id;
+      const query = {
+        _id: new ObjectId(id)
+      };
+      const result = await usersCollection.findOne(query);
+      // console.log(result)
+      res.send(result);
     })
 
+    // my-ideas page 
+    app.get('/my-ideas/:email', verifyToken, async (req, res) => {
+      const email = req.params.email;
+      const query = { userEmail: email };
+      const result = await usersCollection.find(query).toArray();
+      res.send(result);
+    });
+    //  my-interactions
+  app.get('/my-interactions/:email', async(req, res)=>{
+    const email = req.params.email;
+    const query ={
+      userEmail:  email
+    };
+    const result = await usersCollection.find(query).toArray();
+    res.send(result);
+  })
+
+    // 
+    app.patch('/ideas/:id', verifyToken, async (req, res) => {
+      const filter = { _id: new ObjectId(req.params.id) };
+      const updatedDoc = { $set: req.body };
+      const result = await usersCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    });
+
+
+    // delete
+    app.delete('/ideas/:id', verifyToken, async (req, res) => {
+      const result = await usersCollection.deleteOne({ _id: new ObjectId(req.params.id) });
+      res.send(result);
+    });
+
     // post 
-    app.post('/users', async(req, res)=>{
-        const users = req.body;
-        const result = await usersCollection.insertOne(users);
-        // console.log(result);
-        res.send(result);
+    app.post('/users', verifyToken, async (req, res) => {
+      const users = req.body;
+      const result = await usersCollection.insertOne(users);
+      // console.log(result);
+      res.send(result);
     })
 
 
